fc4d13f55b24627a850721ae6d747d841e2055e4
MoeinSorkhei <m.moein.sorkhei@gmail.com>
2020-05-15 13:01:44 +0200

    scripts added

---
+++ scripts/eval.sh
@@ -0,0 +1,12 @@
+python3 main.py --eval \
+        --model resnet --lr 2e-6 --epoch 8 \
+        --data_folder /local_storage/datasets/moein/Thorax/extracted/images \
+        --checkpoints_path /Midgard/home/sorkhei/Thorax/checkpoints  \
+        --results_path /Midgard/home/sorkhei/Thorax/results
+
+
+python3 main.py --eval \
+        --model resnet --freezed --lr 2e-6 --epoch 20 \
+        --data_folder /local_storage/datasets/moein/Thorax/extracted/images \
+        --checkpoints_path /Midgard/home/sorkhei/Thorax/checkpoints  \
+        --results_path /Midgard/home/sorkhei/Thorax/results
